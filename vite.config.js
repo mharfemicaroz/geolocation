6512f9be9679d014c05957df96fd919a3a93ecba
unknown <mharfemicaroz@gmail.com>
2023-08-13 16:37:50 +0800

    fix map

--- vite.config.js
+++ vite.config.js
@@ -12,7 +12,7 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
+      "@": fileURLToPath(new URL("./geolocation/src", import.meta.url)),
     },
   },
   base: "./geolocation/",
